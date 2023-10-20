cc58b84894cba77868f3115cf90250dab7cf51df
David Khauan <125144822+davidkhauan@users.noreply.github.com>
2023-10-20 19:28:48 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,49 @@
+// Codificação simples.
+/*
+const botoes = document.querySelectorAll (".botao");
+const personagens = document.querySelectorAll (".personagem")
+
+botoes.forEach ((botao, indice) => {
+    botao.addEventListener ("click", () => {
+        const botaoSelecionado = 
+        document.querySelector (".botao.selecionado")
+
+        botaoSelecionado.classList.remove ("selecionado")
+        botao.classList.add ("selecionado")
+        
+        const personagemSelecionado = 
+        document.querySelector (".personagem.selecionado")
+
+        personagemSelecionado.classList.remove ("selecionado")
+        personagens [indice].classList.add ("selecionado")
+    })
+})
+*/
+
+//Codificação Refatorada.
+const botoes = document.querySelectorAll (".botao");
+const personagens = document.querySelectorAll (".personagem")
+
+botoes.forEach ((botao, indice) => {
+    botao.addEventListener ("click", () => {
+        desselecionarBotao();
+        desselacionarPersonagem();
+        
+        botao.classList.add ("selecionado");
+        personagens [indice].classList.add ("selecionado");
+    });
+});
+
+function desselacionarPersonagem() {
+    const personagemSelecionado = 
+    document.querySelector(".personagem.selecionado");
+
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = 
+    document.querySelector(".botao.selecionado");
+
+    botaoSelecionado.classList.remove("selecionado");
+};
